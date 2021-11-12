4239808f64f798ccf36ef17c40f52b8eb8ef9481
Romy Alula <goumiesone.dev@gmail.com>
2021-11-12 01:31:26 +0100

    Add checker

---
+++ src/modules/checker.js
@@ -0,0 +1,7 @@
+export const isRedFirst = (/* rainbow */) => {
+  return undefined;
+};
+
+export const isValidRainbow = (/* rainbow */) => {
+  return undefined
+}
---
+++ src/modules/checker.test.js
@@ -0,0 +1,14 @@
+import {isRedFirst} from "./checker";
+
+describe("isRedFirst", () => {
+  it("returns invalid rainbow, given a rainbow without colors", () => {
+    // Arrange
+    const rainbow = [];
+
+    // Act
+    let actualInvalidRainbow = isRedFirst(rainbow);
+
+    // Assert
+    expect(actualInvalidRainbow).toBe(false);
+  });
+});
